2fb7a45c661c112027c3a07fc7d03c92f68d746d
iamareebjamal <jamal.areeb@gmail.com>
2020-10-30 06:19:32 +0530

    fix: Restrict event attendee API

--- app/api/helpers/query.py
+++ app/api/helpers/query.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_rest_jsonapi.exceptions import ObjectNotFound
 
 from app.api.helpers.db import safe_query_kwargs
+from app.api.helpers.errors import ForbiddenError
 from app.api.helpers.permission_manager import has_access
 from app.models.event import Event
 from app.models.role import Role
@@ -17,7 +18,8 @@ def event_query(
 ):
     """
     Queries the event according to 'event_id' and 'event_identifier' and joins for the query
-    For draft events, if the user is not logged in or does not have required permissions, a 404 is raised
+    For draft events, a 404 is raised
+    If the user is not logged in or does not have required permissions, 403 is raised
     :param event_id: String representing event_id in the view_kwargs
     :param event_identifier: String representing event_identifier in the view_kwargs
     :param query_: Query object
@@ -25,26 +27,24 @@ def event_query(
     :param permission: the name of the permission to be applied as a string. Default: is_coorganizer
     :return:
     """
+    event = None
     if view_kwargs.get(event_id):
         event = safe_query_kwargs(Event, view_kwargs, event_id)
-        if event.state != 'published' and (
-            'Authorization' not in request.headers
-            or not has_access(permission, event_id=event.id)
-        ):
+    elif view_kwargs.get(event_identifier):
+        event = safe_query_kwargs(Event, view_kwargs, event_identifier, 'identifier')
+
+    if event:
+        if event.state != 'published':
             raise ObjectNotFound(
                 {'parameter': event_id},
                 "Event: {} not found".format(view_kwargs[event_id]),
             )
-        query_ = query_.join(Event).filter(Event.id == event.id)
-    elif view_kwargs.get(event_identifier):
-        event = safe_query_kwargs(Event, view_kwargs, event_identifier, 'identifier')
-        if event.state != 'published' and (
-            'Authorization' not in request.headers
-            or not has_access(permission, event_id=event.id)
+        if 'Authorization' not in request.headers or not has_access(
+            permission, event_id=event.id
         ):
-            raise ObjectNotFound(
-                {'parameter': event_identifier},
-                "Event: {} not found".format(view_kwargs[event_identifier]),
+            raise ForbiddenError(
+                {'parameter': event_id},
+                "You don't have access to event {}".format(view_kwargs[event_id]),
             )
         query_ = query_.join(Event).filter(Event.id == event.id)
     return query_
